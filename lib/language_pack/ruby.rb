1f9818fa479c52e777f0769e365358f1203b40ab
Nick Bolt <bolt.shadow@gmail.com>
2017-06-08 14:08:29 +0100

    Reduce bundler jobs

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -546,7 +546,7 @@ WARNING
         bundle_without = env("BUNDLE_WITHOUT") || default_bundle_without
         bundle_bin     = "bundle"
         bundle_command = "#{bundle_bin} install --without #{bundle_without} --path vendor/bundle --binstubs #{bundler_binstubs_path}"
-        bundle_command << " -j4"
+        bundle_command << " -j1"
 
         if File.exist?("#{Dir.pwd}/.bundle/config")
           warn(<<-WARNING, inline: true)
